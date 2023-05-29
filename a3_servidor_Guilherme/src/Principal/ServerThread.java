ad4cffbe758ee141874be41f89d1f80b90f5ab5b
Guilherme Fernandes <guifernandes111@gmail.com>
2023-05-29 19:28:02 -0300

    Update ServerThread.java
    Arquivo atualizado devido alguns erros

--- a3_servidor_Guilherme/src/Principal/ServerThread.java
+++ a3_servidor_Guilherme/src/Principal/ServerThread.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.util.Iterator;
 import java.util.List;
 
 public class ServerThread extends Thread {
@@ -22,69 +21,67 @@ public class ServerThread extends Thread {
 
     @Override
     public void run() {
-        while (true) {
-            try {
-                // Fluxo de entrada e saída de dados 
-                ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-                ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+        try {
+            out = new ObjectOutputStream(socket.getOutputStream());
+            in = new ObjectInputStream(socket.getInputStream());
 
-                while (true) {
-                    // Recebe o processo que o cliente escolheu (1-5)
-                    int escolha = in.readInt();
-                    switch (escolha) {
-                        case 1 -> {
-                            // Adicionar
-                            Livro livro = (Livro) in.readObject(); // Recebe o livro do cliente
-                            livroAcervo.add(livro); // Adiciona livro ao array
-                            out.writeObject(livro); // Envia o livro de volta para o cliente
-                            out.flush();
+            while (true) {
+                int escolha = in.readInt();
+                switch (escolha) {
+                    case 1:
+                        // Adicionar
+                        Livro livro = (Livro) in.readObject();
+                        livroAcervo.add(livro);
+                        out.writeObject(livro);
+                        out.flush();
+                        break;
 
+                    case 2:
+                        // Atualizar
+                        Livro livroAtualizado = (Livro) in.readObject();
+                        boolean atualizado = Livro.foiAtualizado(livroAcervo, livroAtualizado);
+                        if (atualizado) {
+                            out.writeObject(livroAtualizado);
+                        } else {
+                            out.writeObject(null);
                         }
-                        case 2 -> {
-                            // Atualizar
-                            Livro livroAtualizado = (Livro) in.readObject(); // Recebe as informações atualizadas do livro
-                            boolean atualizado = Livro.foiAtualizado(livroAcervo, livroAtualizado);
-                            if (atualizado) {
-                                out.writeObject(livroAtualizado); // Envia o livro atualizado
-                            } else {
-                                out.writeObject(null);
-                            }
-                            out.flush();
+                        out.reset(); // Reinicializa o fluxo de saída
+                        out.flush(); // Limpa o buffer e garante o envio dos dados
 
-                        }
-                        case 3 -> {
-                            // Remover
-                            int id = in.readInt();
-                            boolean removed = remover(livroAcervo, id);
-                            out.writeBoolean(removed); // Indica se o livro foi removido com sucesso
-                            out.flush();
-                        }
-                        case 4 -> {
-                            // Consultar
-                            int id = in.readInt(); // Recebe o ID do livro que será consultado
-                            Livro livroConsultado = Livro.consultarId(livroAcervo, id); // Pesquisa o livro no array pelo ID
-                            out.writeObject(livroConsultado); // Envia o livro consultado para o cliente
-                            out.flush();
+                        break;
 
-                        }
-                        case 5 -> {
-                            // Sair
-                            break;
-                        }
-                    }
+                    case 3:
+                        // Remover
+                        int id = in.readInt();
+                        boolean removed = remover(livroAcervo, id);
+                        out.writeBoolean(removed);
+                        out.flush();
+                        break;
+
+                    case 4:
+                        // Consultar
+                        int idConsulta = in.readInt();
+                        Livro livroConsultado = Livro.consultarId(livroAcervo, idConsulta);
+                        out.writeObject(livroConsultado);
+                        out.flush();
+                        break;
+
+                    case 5:
+                        // Sair
+                        break;
                 }
 
-            } catch (IOException | ClassNotFoundException e) {
-                System.out.println("Exceção: " + e.getMessage());
+                if (escolha == 5) {
+                    // Fecha os fluxos de entrada e saída de dados
+                    out.close();
+                    in.close();
+                    socket.close();
+                    break;
+                }
             }
-            try {
-                // Fecha os fluxos de entrada e saída de dados
-                out.close();
-                in.close();
-                socket.close();
-            } catch (IOException e) {
-                System.out.println("Exceção ao fechar os fluxos: " + e.getMessage());
-            }
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("Exceção: " + e.getMessage());
         }
     }
-}    
+
+}
